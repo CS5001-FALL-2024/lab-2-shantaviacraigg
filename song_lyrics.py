d36026443efe9470353a5ce13754e4e24fab994b
shantaviacraigg <craigg.s@northeastern.edu>
2024-09-18 18:36:13 -0700

    added quotes to fix error

--- song_lyrics.py
+++ song_lyrics.py
@@ -10,22 +10,19 @@ Choose 10 words of your favorite song lyrics (safe for work only please :)
 Store each word of the lyric in a separate variable, then print the entire 
 lyric on one single line using print.
 '''
-
+w1="Love, "
+w2="never "
+w3="knew "
+w4="what "
+w5="I "
+w6="was "
+w7="missin', "
+w8="but "
+w9="I "
+w10="knew "
 def main():
+    print(w1+w2+w3+w4+w5+w6+w7+w8+w9+w10)
     
-    w1=love
-    w2=never 
-    w3=knew 
-    w4=what 
-    w5=I
-    w6=was
-    w7=missin
-    w8=but 
-    w9=I 
-    ww1=knew
-
-    print(w1+w2+w3+w4+w5+w6+w7+w8+w9+ww1)
-
 
 if __name__ == '__main__':
     main()
